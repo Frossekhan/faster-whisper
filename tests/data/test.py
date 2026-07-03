c7c25f3490f437b250bc64426f8562e105efd422
Frossekhan <138743885+Frossekhan@users.noreply.github.com>
2026-07-03 18:38:13 +0530

    hi

---
+++ tests/data/test.py
@@ -0,0 +1,14 @@
+from faster_whisper import WhisperModel
+
+model = WhisperModel(
+    "base",
+    device="cpu",
+    compute_type="int8"
+)
+
+segments, info = model.transcribe("speech.wav")
+
+print("Language:", info.language)
+
+for segment in segments:
+    print(segment.text)
